efde26a4223a4e5f130aff4e4f2dde13806f02ce
CiarduLucas <lucas.m.ciardullo@gmail.com>
2024-08-10 01:31:16 -0300

    Subo generador

--- 2024/Algoritmos y estructuras de datos/TP Anual/generador.cpp
+++ 2024/Algoritmos y estructuras de datos/TP Anual/generador.cpp
@@ -13,7 +13,7 @@ struct Usuario{
 };
 
 struct Transaccion{
-    long int id;
+    int id;
     int monto;
     bool ingreso;
     int fecha; //ddmmaaaa
@@ -21,12 +21,18 @@ struct Transaccion{
 
 
 bool signIn(char username[50], char loginCode[50], Usuario &user);
+Transaccion transaccion1(Transaccion operacion);
+int menu();
+void cargarValores(Transaccion &t);
 
 int main(){
     char username[50];
     char loginCode[50];
+    int opcion;
     int flag = 0;
     Usuario user;
+    Transaccion t = {0, 0, true, 0};
+
     
     do{
         if (flag == 1){
@@ -40,30 +46,97 @@ int main(){
 
         flag = 1; 
     }while(!signIn(username, loginCode, user));
+    
     cout<< "Balance actual de la cuenta es de $ "<< user.balance <<endl;
+
+    do{
+        opcion = menu();
+        switch(opcion){
+            case 1:
+                cargarValores(t);
+                transaccion1(t);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                cout<<"Valor incorrecto"<< endl;
+                break;
+        }
+    }while(opcion!=5);
     
 
     return 0;
 }
 
-Transaccion agregartransaccion(Transaccion operacion){
+void cargarValores(Transaccion &t){
+    int flag = 0;
+    int mes, año;
+
+    do{
+        if(flag){
+            cout<< "El monto no puede ser negativo: ";
+            flag = 0;
+        }
+        else{
+            cout<< "Ingrese el monto: ";
+        }
+        cin>> t.monto;
+        flag = 1;
+    }while(t.monto<= 0);
+
+    flag = 0;
     
+    do{
+        if(flag==1){
+            cout<<"La fecha es incorrecta: "<< endl;    
+            cin>> t.fecha;
+            flag = 0;
+        }
+        else{
+            cout<<"Ingrese la fecha: "<< endl;
+            cin>> t.fecha;
+        }
+        mes = t.fecha % 1000000;
+        mes = mes / 10000;
+        año = t.fecha % 10000; 
+        if ((t.fecha / 1000000 > 31) || (t.fecha / 1000000 <= 0) || (mes>12) || (mes<=0) || ( año>9999)|| (año<=0)) {
+            flag = 1;
+        } 
+
+    }while(flag);   
+}
+
+Transaccion transaccion1(Transaccion operacion){
+    Transaccion idValidation;
     const char* nombreArchivo = "transacciones.dat";
-    FILE* archivo = fopen(nombreArchivo, "ab");
+    
+    FILE * archivo = fopen(nombreArchivo, "rb");
     if (archivo == NULL) {
-        cout << "Error al abrir el archivo." << endl;
-        return operacion;
+        operacion.id = 1;
+        fclose(archivo);
     }
-    fwrite(&operacion, sizeof(Usuario), 1, archivo);
+    else{
+        fseek(archivo, -sizeof(Transaccion), SEEK_END);
+        if(fread(&idValidation, sizeof(Transaccion), 1, archivo) == 1){
+            operacion.id=idValidation.id + 1;
+            fclose(archivo);
+        }
+    }
+
+
+    archivo = fopen(nombreArchivo, "ab");
+    fwrite(&operacion, sizeof(Transaccion), 1, archivo);
     fclose(archivo);
-    return operacion;
+    return idValidation;
 }
 
 bool signIn(char username[50],char loginCode[50], Usuario &user) {
     bool ingresoValido = false;
 
     FILE* archivo = fopen("archivo.dat", "rb");
-    if (archivo != nullptr) {
+    if (archivo != NULL) {
         while (fread(&user, sizeof(Usuario), 1, archivo) == 1) {
             if (strcasecmp(user.username, username) == 0) {
                 if (strcasecmp(user.loginCode, loginCode) == 0) {
@@ -75,5 +148,18 @@ bool signIn(char username[50],char loginCode[50], Usuario &user) {
         fclose(archivo);
     }
     return ingresoValido;
+}
+
+int menu(){
+    int opcion = 0;
+    cout<<"1. Ingresar dinero"<< endl;
+    cout<<"2. Egresar Dinero"<< endl;
+    cout<<"3. Listar transacciones"<<endl;
+    cout<<"4. Eliminar transaccion"<<endl;
+    cout<<"5. Salir"<<endl;
+    cout << "Seleccione una opción: ";
+    cin>>opcion;
+
+    return opcion;
 }
 
